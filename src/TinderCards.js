3c4400ef0b5845cc22c177a5e37f957c3f014771
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-15 23:47:28 +0000

    ADD React Tinder Compo

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import "./TinderCards.css";
+import TinderCard from "react-tinder-card";
 
 function TinderCards() {
-  return <div className="tinderCards"></div>;
+  const [people, setPeople] = useState([
+    {
+      name: "Elon Musk",
+      url:
+        "https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg",
+    },
+    {
+      name: "Jeff Bezoz",
+      url:
+        "https://uploads-ssl.webflow.com/5dd3f4647030aabbc681ce89/5dd805edb66d23eedfa0de75_416x416%20(9).jpg",
+    },
+  ]);
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+  };
+
+  const outOfFrame = (person) => {
+    console.log("left the screen!");
+  };
+
+  return (
+    <div className="tinderCards">
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+            onSwipe={(dir) => swiped(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default TinderCards;
