38c138042227090de87101332be3508ad57a48ba
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-15 23:51:21 +0000

    ADD data to Cards

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -35,7 +35,12 @@ function TinderCards() {
             onSwipe={(dir) => swiped(dir, person.name)}
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
-            <div className=""></div>
+            <div
+              style={{ backgroundImage: `url(${person.url})` }}
+              className="card"
+            >
+              <h3>{person.name}</h3>
+            </div>
           </TinderCard>
         ))}
       </div>
