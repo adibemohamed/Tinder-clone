b20d6977d0851899ddee40da56e225b34ba05cfc
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-16 18:34:29 +0000

    SETUP API endpoints

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import Cards from "./dbCards.js";
+import Cors from "cors";
 
 // App Config
 const app = express(); // create app instance
@@ -7,6 +9,8 @@ const port = process.env.PORT || 8000;
 const connection_url = `mongodb+srv://admin:jbGoFSxLJ3LtvIh4@cluster0.weytf.mongodb.net/tinderdb?retryWrites=true&w=majority`;
 
 // Middlewares
+app.use(express.json());
+app.use(Cors());
 
 // DB config
 mongoose.connect(connection_url, {
@@ -18,6 +22,28 @@ mongoose.connect(connection_url, {
 // API Endpoints
 app.get("/", (req, res) => {
   res.status(200).send("HELLO CLEAVER PROGRAMMERS");
+});
+
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/tinder/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
 });
 
 // Listener
