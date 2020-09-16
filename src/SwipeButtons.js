dd2e45acbe20f5f477525c0cec0a4470a9749da5
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-16 15:49:09 +0000

    ADD SwipeButtons compo

---
+++ src/SwipeButtons.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SwipeButtons.css";
+
+function SwipeButtons() {
+  return (
+    <div className="swipeButtns">
+      <h1>I am swipe buttons</h1>
+    </div>
+  );
+}
