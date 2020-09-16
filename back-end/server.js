4b5e0ede39137dedf1ed7600d988ade244e45192
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-16 18:14:48 +0000

    CONFIG MongoDB connection

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,19 @@
 import express from "express";
-import mogoose from "mongoose";
+import mongoose from "mongoose";
 
 // App Config
 const app = express(); // create app instance
 const port = process.env.PORT || 8000;
+const connection_url = `mongodb+srv://admin:jbGoFSxLJ3LtvIh4@cluster0.weytf.mongodb.net/tinderdb?retryWrites=true&w=majority`;
 
 // Middlewares
 
 // DB config
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 // API Endpoints
 app.get("/", (req, res) => {
